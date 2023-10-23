6b306c56b885c346150c9a056e66beb3ba54649f
Camden Hess <camden.hess12@gmail.com>
2023-10-23 15:45:39 -0600

    'finished module project'

--- frontend/index.js
+++ frontend/index.js
@@ -74,12 +74,12 @@ function moduleProject2() {
     let isDown = evt.key === keys.down
     let isRight = evt.key === keys.right
     let isLeft = evt.key === keys.left
+    let isSpacebar = evt.key === keys.space
 
     let targeted = document.querySelector('.targeted')
     let idx = Array.from(targeted.parentElement.children).indexOf(targeted)
 
     if (isUp) {
-      console.log('You clicked up')
       if (targeted.parentElement.previousElementSibling) {
         targeted.classList.remove('targeted')
         targeted.parentElement.previousElementSibling.children[idx].classList.add('targeted')
@@ -102,9 +102,28 @@ function moduleProject2() {
     }
 
     // 👉 TASK 4 - Use the space bar to exterminate a mosquito 👈
+    else if (isSpacebar) {
+      let mosquito = targeted.firstChild
+      if (mosquito && mosquito.dataset.status === 'alive') {
+        mosquito.dataset.status = 'dead'
+        mosquito.parentElement.style.backgroundColor = 'red'
+      }
+      // 👉 TASK 5 - End the game 👈
+      let liveMosquitos = document.querySelectorAll('[data-status=alive]')
+      if (!liveMosquitos.length) {
+        let elapsed = getTimeElapsed()
+        document.querySelector('p.info').textContent = `Extermination completed in ${elapsed/1000} seconds!`
 
-    // 👉 TASK 5 - End the game 👈
-  })
+        let restartBtn = document.createElement('button')
+        restartBtn.textContent = 'Restart'
+        restartBtn.addEventListener('click', () => {
+          location.reload()
+        })
+        document.querySelector('h2').insertAdjacentElement('beforeend', restartBtn)
+      }
+    }
+      
+    })
   // 👆 WORK WORK ABOVE THIS LINE 👆
 }
 
